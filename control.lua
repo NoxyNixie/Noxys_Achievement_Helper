0e14539a8d14153957ea829998a96645583ce25f
Mark Sanders <mark.something@gmail.com>
2018-02-24 20:24:17 +0100

    Initial version. Supports 3 achievements.

---
+++ control.lua
@@ -0,0 +1,26 @@
+local dont_build = require("dont_build")
+
+script.on_event(defines.events.on_built_entity, function(e)
+	if e.created_entity.type == "entity-ghost" and dont_build[e.created_entity.ghost_name] then
+		if e.player_index then
+			game.players[e.player_index].print("Achievement Helper: Blocking: " .. e.created_entity.ghost_name)
+		end
+		e.created_entity.destroy()
+	end
+end)
+
+script.on_event(defines.events.on_player_cursor_stack_changed, function(e)
+	local player = game.players[e.player_index]
+	if player.cursor_stack and player.cursor_stack.valid_for_read and dont_build[player.cursor_stack.name] then
+		local i = {name = player.cursor_stack.name, count = player.cursor_stack.count}
+		for _,v in pairs{defines.inventory.player_quickbar, defines.inventory.player_main, defines.inventory.player_trash, defines.inventory.player_vehicle} do
+			local inv = player.get_inventory(v)
+			if inv.can_insert(i) then
+				player.cursor_stack.clear()
+				inv.insert(i)
+				player.print("Achievement Helper: Blocking: " .. i.name)
+				break
+			end
+		end
+	end
+end)
---
+++ settings.lua
@@ -0,0 +1,28 @@
+data:extend({
+	-- Startup
+	{
+		type = "bool-setting",
+		name = "Noxys_Achievement_Helper-raining-bullets",
+		setting_type = "startup",
+		default_value = false,
+		order = "a",
+	},
+	{
+		type = "bool-setting",
+		name = "Noxys_Achievement_Helper-logistic-network-embargo",
+		setting_type = "startup",
+		default_value = false,
+		order = "b",
+	},
+	{
+		type = "bool-setting",
+		name = "Noxys_Achievement_Helper-steam-all-the-way",
+		setting_type = "startup",
+		default_value = false,
+		order = "b",
+	},
+	-- Global
+
+	-- Per user
+
+})
