26b67a05e40e403ebb515a2ef619d9646852d09d
Mark Sanders <mark.something@gmail.com>
2018-02-24 23:39:30 +0100

    Removed debug code. Added support for god* inventories (some mods use this).

--- control.lua
+++ control.lua
@@ -13,9 +13,16 @@ script.on_event(defines.events.on_player_cursor_stack_changed, function(e)
 	local player = game.players[e.player_index]
 	if player.cursor_stack and player.cursor_stack.valid_for_read and dont_build[player.cursor_stack.name] then
 		local i = {name = player.cursor_stack.name, count = player.cursor_stack.count}
-		for _,v in pairs{defines.inventory.player_quickbar, defines.inventory.player_main, defines.inventory.player_trash, defines.inventory.player_vehicle} do
+		for _,v in pairs{
+			defines.inventory.player_quickbar,
+			defines.inventory.player_main,
+			defines.inventory.player_trash,
+			defines.inventory.player_vehicle,
+			defines.inventory.god_quickbar,
+			defines.inventory.god_main
+		} do
 			local inv = player.get_inventory(v)
-			if inv.can_insert(i) then
+			if inv and inv.can_insert(i) then
 				player.cursor_stack.clear()
 				inv.insert(i)
 				player.print("Achievement Helper: Blocking: " .. i.name)
--- settings-updates.lua
+++ settings-updates.lua
@@ -1,10 +1,6 @@
 for k,v in pairs(data.raw["bool-setting"]) do
-	log(k .. ":" .. tostring(v))
-end
-for k,v in pairs(data.raw["bool-setting"]) do
 	local _, _, t = k:find("Noxys_Achievement_Helper%-([%a-]+)")
 	if t then
-		log(serpent.block(v))
 		v.localised_name = {"mod-setting-name.Noxys_Achievement_Helper-assist", {"achievement-name." .. t}}
 	end
 end
